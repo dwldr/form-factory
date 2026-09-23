ec47afa4fbfd422181848f8718e27fefeecf86ee
Derek Wilder <derekwilder@gmail.com>
2026-09-23 16:45:33 -0400

    more starter content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,21 @@
 import {Component} from '@angular/core';
+import {Home} from './home/home';
 
 @Component({
   selector: 'app-root',
-  template: ` Welcome to Angular! `,
+  imports: [Home],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home />
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.css'],
 })
-export class App {}
+export class App {
+  title = 'homes';
+}
---
+++ src/app/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-home',
+  styles: ``,
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+})
+export class Home {
+}
